3fa5bfe7a265657d739105efcfdf43610d50854d
xie0027 <30735172+xie0027@users.noreply.github.com>
2025-04-23 23:47:24 +0800

    Update cy.js

--- cy.js
+++ cy.js
@@ -1,30 +1,9 @@
-const url = $request.url;
-const isRequest = typeof $request !== "undefined";
+let body = $response.body;
+let obj = JSON.parse(body);
 
-if (isRequest) {
-    // 拦截请求
-    if (/v1\/(satellite|nafp\/origin_images)/.test(url)) {
-        const headers = $request.headers;
-        headers['device-token'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNzA1MzMxMTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.h_Cem89QarTXxVX9Z_Wt-Mak6ZHAjAJqgv3hEY6wpps';
-        $done({ headers });
-    } else {
-        $done({});
-    }
-} else {
-    // 拦截响应
-    let body = $response.body;
-
-    if (url.includes("/v2/user")) {
-        const obj = JSON.parse(body);
-        obj.result.is_vip = true;
-        obj.result.vip_type = "s";
-        obj.result.svip_expired_at = 4092599349;
-        body = JSON.stringify(obj);
-    }
-
-    if (url.includes("v1/activity")) {
-        body = '{"status":"ok","activities":[{"items":[{}]}]}';
-    }
-
-    $done({ body });
+// 只修改 svip 剩余时间
+if (obj.svip && typeof obj.svip === "object") {
+    obj.svip.expires_time = "4092599349";
 }
+
+$done({ body: JSON.stringify(obj) });
